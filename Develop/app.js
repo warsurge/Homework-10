2ec674dabbbca4b7d8b2dd04b6430e89496a7410
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-04-15 21:02:26 -0500

    update html

--- Develop/app.js
+++ Develop/app.js
@@ -4,16 +4,16 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​function promptManager(){
+function promptManager() {
     return inquirer.prompt([
         {
             type: "input",
@@ -22,7 +22,7 @@ const render = require("./lib/htmlRenderer");
         },
         {
             type: "input",
-            name:"id",
+            name: "id",
             message: "What is your ID number?"
         },
         {
@@ -35,59 +35,67 @@ const render = require("./lib/htmlRenderer");
             name: "office",
             message: "What is your office number?"
         }])
-        
+
+}
+const team = []
+
+function promptTeam() {
+    if (job === "engineer") {
+        function promptEngineer() {
+            [
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is your name?"
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "What is your ID number?"
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "What is your email?"
+                },
+                {
+                    type: "input",
+                    name: "github",
+                    message: "What is your GitHub username?"
+                }]
+                team.push(new Engineer(name,id,email,github))
+            return team()
+        }
+    } else if (role === "intern") {
+
+        function promptIntern() {
+            return inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is your name?"
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "What is your ID number?"
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "What is your email?"
+                },
+                {
+                    type: "input",
+                    name: "school",
+                    message: "Where did you graduate from?"
+                }]
+            )
+        }
+    }
 }
 
-function promptEngineer(){[
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name:"id",
-        message: "What is your ID number?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?"
-    }]
-    return inquirer.prompt()
-}
-
-function promptIntern(){
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "input",
-            name:"id",
-            message: "What is your ID number?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email?"
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "Where did you graduate from?"
-        }]
-        )
-}
-
-function promptEmployee(){
+function promptEmployee() {
 
     return inquirer.prompt(
         {
@@ -95,24 +103,24 @@ function promptEmployee(){
             name: "title",
             message: "Do you want to add another Employee?",
             choices: ["I Don't Want to Add Someone", "Engineer", "Intern"]
-    
+
         }
-        )
-    }
+    )
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
